f5d47b9975d66276a18fa8fa339ebfa38ef90634
Diana Gelinas <dianablythegelinas@gmail.com>
2022-04-11 15:48:48 -0600

    Add Javascript 2

---
+++ script.js
@@ -0,0 +1,19 @@
+let title = document.getElementById("name");
+
+title.addEventListener("mouseover", function () {
+    title.style.color = "purple";
+});
+
+title.addEventListener("mouseout", function () {
+    title.style.color = "darkturquoise";
+});
+
+let content = document.getElementById("content");
+
+content.addEventListener("mouseover", function () {
+    content.style.color = "purple";
+});
+
+content.addEventListener("mouseout", function () {
+    content.style.color = "darkturquoise";
+});
